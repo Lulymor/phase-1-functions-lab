4f17c5b35df42d4d9fd24caa5320186c1942288b
Luana <lugamoreira@gmail.com>
2022-12-14 19:29:44 -0500

    commit tree 0809169c53cdcb5c5c83543cfe9165a03b9edade

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (location) => Math.abs(location - 42);
+//Math.abs returns the absoute value of a number, no negative numbers  
+// const theDistance = distanceFromHqInBlocks(34)
+// console.log(`The distance is ${theDistance}`)
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(start - destination)
+    return distance * 264
+    
+}
+function calculatesFarePrice(start, destination) {
+ const distance = distanceTravelledInFeet(start, destination)
+    if(distance <= 400){
+        return 0
+    }
+    else if(distance > 400 && distance <= 2000){
+        return (distance - 400) * 0.02
+    }
+    else if(distance > 2000 && distance <= 2500){
+        return 25
+}
+else{
+    return `cannot travel that far`
+}}
